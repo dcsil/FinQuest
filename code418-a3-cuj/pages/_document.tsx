2cb5a45aed19c4b45b58ecf763248b1588ad7381
Jason Wang <40612523+jasonfyw@users.noreply.github.com>
2025-09-22 19:34:59 -0400

    add: mantine

--- code418-a3-cuj/pages/_document.tsx
+++ code418-a3-cuj/pages/_document.tsx
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from 'next/document';
+import { ColorSchemeScript, mantineHtmlProps } from '@mantine/core';
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="en" {...mantineHtmlProps}>
+            <Head>
+                <ColorSchemeScript defaultColorScheme="auto" />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
